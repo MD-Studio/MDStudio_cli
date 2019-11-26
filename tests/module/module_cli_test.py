ec4e4bffce8e2e9d71c0d2d375c7f93c1c2165fd
Marc van Dijk <marcvdijk@gmail.com>
2019-11-26 11:24:14 +0100

    Add unittests boilerplate code

---
+++ tests/module/module_cli_test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Unit tests for MDStudio CLI methods
+"""
+
+import unittest
+
+
+class MDStudioCliTests(unittest.TestCase):
+
+    def test_requires_test(self):
+
+        print('We do realy need some tests here :-)')
+        self.assertTrue(True)

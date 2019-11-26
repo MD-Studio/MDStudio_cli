0eecffe6e7f8a03945d97b8c085474a098a08688
Marc van Dijk <marcvdijk@gmail.com>
2019-11-26 11:14:48 +0100

    Update Github repo URL

--- mdstudio_cli/__init__.py
+++ mdstudio_cli/__init__.py
@@ -15,6 +15,6 @@ __author__ = 'Marc van Dijk'
 __status__ = 'pre-release beta1'
 __date__ = '15 april 2016'
 __licence__ = 'Apache Software License 2.0'
-__url__ = 'https://github.com/MD-Studio/MDStudio'
+__url__ = 'https://github.com/MD-Studio/MDStudio_cli'
 __copyright__ = "Copyright (c) VU University, Amsterdam"
 __rootpath__ = os.path.dirname(__file__)
